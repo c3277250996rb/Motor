56f53667de78d838458ebdea46464c9d5a2fb3fe
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2026-07-25 20:11:25 +0800

    4.4.3 单通道 ADC 采集（DMA 读取）

---
+++ 0725/4.4.3_单通道 ADC 采集（DMA 读取）/Core/Code/Main/test.c
@@ -0,0 +1,15 @@
+/*金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮*/
+#include "code.h"
+
+
+#if 723
+
+extern __IO uint16_t IC2Value;
+extern __IO uint16_t IC1Value;
+extern __IO float DutyCycle;
+extern __IO float Frequency;
+int my_test(void){
+    
+}
+#endif
+
