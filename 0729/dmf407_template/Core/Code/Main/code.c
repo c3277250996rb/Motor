9bc47add14ebd580b5d3f7b4cfd509e882bc74c2
c3277250996rb <138227915+c3277250996rb@users.noreply.github.com>
2026-07-29 23:29:17 +0800

    .

---
+++ 0729/dmf407_template/Core/Code/Main/code.c
@@ -0,0 +1,42 @@
+/*金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮金米妮*/
+#include "code.h"
+
+void CODE_MAIN(void){
+    CODE_INIT();
+    POWER_ON();
+    CODE_SELF_TEST();
+    CODE_WHILE();
+}
+
+
+void CODE_INIT(void){
+	delay_init(di_sysclk);      //初始化延时函数
+    uart_init(115200);          //初始化串口
+    usmart_init(ui_sysclk);     //初始化USMART
+}
+
+void CODE_SELF_TEST(void){
+    while(1){
+        // muti_printf_self_test();
+        // my_led_self_test();
+        while(1);
+    }
+}
+
+void CODE_WHILE(void){
+
+    while(1){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
